7b9e64caff58efc77a78cb6cf3bf15d86bb37a69
flyyy <flyyy1024@gmail.com>
2018-12-28 17:13:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/flyyy/musicplayertest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.flyyy.musicplayertest;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button playButton = findViewById(R.id.play_button);
+        Button pauseButton = findViewById(R.id.pause_button);
+        mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.lasts_ummer);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
